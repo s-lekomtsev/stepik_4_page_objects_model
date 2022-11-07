a7ae88dd78d11bc5cd8ec83b8c8f64695044ff73
s-lekomtsev <lekomtsevsa@gmail.com>
2022-11-07 21:52:29 +0500

    2022-11-07-v4
    Изменения по страницу https://stepik.org/lesson/238819/step/6?unit=211271

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,10 @@
+from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 import time
 
-link = "http://selenium1py.pythonanywhere.com/"
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    time.sleep(5)
-    go_to_login_page(browser)
-    time.sleep(2)
+
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.should_be_login_link()
